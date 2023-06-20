22cb8f48255e61246b7c565b9bbe10a681cd48d6
Vanilonnn <104099919+Vanilonnn@users.noreply.github.com>
2023-06-20 02:58:47 -0700

    Add files via upload

---
+++ SmartSport/SmartSport/Views/Registering.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartSport.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registering : ContentPage
+	{
+		public Registering ()
+		{
+			InitializeComponent ();
+		}
+        private async void Next(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Calendar(), true);
+        }
+    }
+}
---
+++ SmartSport/SmartSport/Views/login.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartSport.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class login : ContentPage
+	{
+		public login ()
+		{
+			InitializeComponent ();
+		}
+        private async void Entering(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Calendar(), true);
+        }
+        private async void Reging(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Registering(), true);
+        }
+    }
+
+}
